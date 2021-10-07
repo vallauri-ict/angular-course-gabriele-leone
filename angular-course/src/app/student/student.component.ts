4fe2d00ae108c02a53cd2d7258b5b667f16bbac5
Gabriele <62541822+gabriele-leone@users.noreply.github.com>
2021-10-07 11:02:25 +0200

    Lesson 1 - Step 1,2,3
    Lesson 1 - Step 3: ngStyle applied on student.component.html
    Lesson 1 - Step 2: student component creatw
    Lessons 1 - Step 1: clear the starting code

---
+++ angular-course/src/app/student/student.component.ts
@@ -0,0 +1,32 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css'],
+})
+export class StudentComponent implements OnInit {
+  studentList = [
+    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M' },
+    { name: 'Giada Valinotto', hobby: 'Volley', gender: 'F' },
+    { name: 'Michele Ghisolfi', hobby: 'Karate', gender: 'M' },
+    { name: 'Luca Pelissero', hobby: 'Paddle', gender: 'M' },
+    { name: 'Martina Velardi', hobby: 'Volley', gender: 'F' },
+    { name: 'Federico Mameli', hobby: 'Paddle', gender: 'M' },
+    { name: 'Lorenzo Cravero', hobby: 'Paddle', gender: 'M' },
+    { name: 'Samuele Detoffol', hobby: 'Paddle', gender: 'M' },
+    { name: 'Marta Baudracco', hobby: 'Karate', gender: 'F' },
+    { name: 'Racca Paolo', hobby: 'Paddle', gender: 'M' },
+    { name: 'Ivan Angjelovski', hobby: 'Karate', gender: 'M' },
+    { name: 'Bianca Teleman', hobby: 'Volley', gender: 'F' },
+  ];
+  student:any;
+  
+  constructor() {
+    let num = Math.floor(Math.random() * this.studentList.length);
+    this.student = this.studentList[num];
+  }
+
+  ngOnInit(): void {}
+}
