028e925d77d2bdc741d26e1a55b545141428dbfd
Gabriele <62541822+gabriele-leone@users.noreply.github.com>
2021-10-14 11:31:37 +0200

    ngIf and ngFor directive and input decorator

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '5 INF B 2021/2022';
+
+  studentList = [
+    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M', isPro: false },
+    { name: 'Giada Valinotto', hobby: 'Volley', gender: 'F', isPro: false },
+    { name: 'Michele Ghisolfi', hobby: 'Karate', gender: 'M', isPro: false },
+    { name: 'Luca Pelissero', hobby: 'Paddle', gender: 'M', isPro: false },
+    { name: 'Martina Velardi', hobby: 'Volley', gender: 'F', isPro: false },
+    { name: 'Federico Mameli', hobby: 'Paddle', gender: 'M', isPro: false },
+    { name: 'Lorenzo Cravero', hobby: 'Paddle', gender: 'M', isPro: false },
+    { name: 'Samuele Detoffol', hobby: 'Paddle', gender: 'M', isPro: false },
+    { name: 'Marta Baudracco', hobby: 'Karate', gender: 'F', isPro: false },
+    { name: 'Paolo Racca', hobby: 'Paddle', gender: 'M', isPro: false },
+    { name: 'Ivan Angjelovski', hobby: 'Karate', gender: 'M', isPro: false },
+    { name: 'Bianca Teleman', hobby: 'Volley', gender: 'F', isPro: false },
+  ];
 }
--- angular-course/src/app/student/student.component.ts
+++ angular-course/src/app/student/student.component.ts
@@ -1,5 +1,5 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-student',
@@ -7,33 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css'],
 })
 export class StudentComponent implements OnInit {
-  studentList = [
-    { name: 'Ettore Esposito', hobby: 'Karate', gender: 'M' },
-    { name: 'Giada Valinotto', hobby: 'Volley', gender: 'F' },
-    { name: 'Michele Ghisolfi', hobby: 'Karate', gender: 'M' },
-    { name: 'Luca Pelissero', hobby: 'Paddle', gender: 'M' },
-    { name: 'Martina Velardi', hobby: 'Volley', gender: 'F' },
-    { name: 'Federico Mameli', hobby: 'Paddle', gender: 'M' },
-    { name: 'Lorenzo Cravero', hobby: 'Paddle', gender: 'M' },
-    { name: 'Samuele Detoffol', hobby: 'Paddle', gender: 'M' },
-    { name: 'Marta Baudracco', hobby: 'Karate', gender: 'F' },
-    { name: 'Racca Paolo', hobby: 'Paddle', gender: 'M' },
-    { name: 'Ivan Angjelovski', hobby: 'Karate', gender: 'M' },
-    { name: 'Bianca Teleman', hobby: 'Volley', gender: 'F' },
-  ];
-  student:any;
-  
-  constructor() {
-   this.randomStudent()
-  }
-  private randomStudent(){
-    let num = Math.floor(Math.random() * this.studentList.length);
-    this.student = this.studentList[num];
-  }
-  ngOnInit(): void {}
+  // student: any;
+  @Input() student:any
 
-  onStudentClick(){
-  this.randomStudent()
+  constructor() {
+  }
+  private randomStudentPro() {
+     let num = Math.floor(Math.random() * 2);
+     if(num==1){
+       this.student.isPro=true;
+     }
+  }
+  ngOnInit(): void { 
+    this.randomStudentPro()
+  }
+
+  onStudentClick() {
+    this.student.isPro=!this.student.isPro
   }
 }
 
